61b3b710fd44ffea5744baf6876481abef1d80ab
Andrew Richardson <andrewrichardson@Andrews-MacBook-Pro.local>
2013-10-01 21:07:47 -0700

    Finally pushing this up

--- config.rb
+++ config.rb
@@ -2,14 +2,13 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
-fonts_dir = "fonts"
+css_dir = "css"
+sass_dir = "css"
+images_dir = "media"
+javascripts_dir = "js"
+fonts_dir = "media/fonts"
 
 output_style = :nested
-environment = :development
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
@@ -23,4 +22,5 @@ color_output = false
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
 # and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+# sass-convert -R --from scss --to sass css scss && rm -rf sass && mv scss sass
+preferred_syntax = :scss
